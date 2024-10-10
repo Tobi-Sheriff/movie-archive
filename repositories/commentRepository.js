8f81cd9b474ad9d05adf8e97789263428f87a1ae
Tobi-Sheriff <57505340+Tobi-Sheriff@users.noreply.github.com>
2024-10-10 19:08:32 +0100

    Enable github actions (#4)
    * testing git actions
    
    * testing get actions 2
    
    * updated index to use env.test file
    
    * removed .env.test from gitignore
    
    * .env.test update

--- repositories/commentRepository.js
+++ repositories/commentRepository.js
@@ -2,6 +2,9 @@ const FileCommentRepository = require('./fileCommentRepository');
 
 function getRepository() {
   const db_type = process.env.DB_TYPE
+  if (!db_type) {
+    throw new Error('DB type not defined');
+  }
   if (db_type == 'file') {
     return new FileCommentRepository();
   }
--- repositories/movieRepository.js
+++ repositories/movieRepository.js
@@ -2,6 +2,9 @@ const FileMovieRepository = require('./fileMovieRepository');
 
 function getRepository() {
   const db_type = process.env.DB_TYPE
+  if (!db_type) {
+    throw new Error('DB type not defined');
+  }
   if (db_type == 'file') {
     return new FileMovieRepository();
   }
