e3c9a3bbfab95c20ddb89f6412d1f7ccc8325d4a
TobiSheriff <s.oluwatobiloba30@gmail.com>
2024-10-19 20:51:26 +0100

    fallback updated to correct datatype

--- config/config.js
+++ config/config.js
@@ -8,10 +8,10 @@ if (env === 'test') {
 
 module.exports = {
     [env]: {
-        username: process.env.DB_USER || postgres,
-        password: process.env.DB_PASSWORD || postgres,
-        database: process.env.DB_NAME || movie_db_test,
-        host: process.env.DB_HOST || localhost,
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        database: process.env.DB_NAME || 'movie_db_test',
+        host: process.env.DB_HOST || 'localhost',
         dialect: 'postgres',
     }
 }
