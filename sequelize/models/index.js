a132b9128eb8d42abbb2bf777a60d79eb1c2790d
TobiSheriff <s.oluwatobiloba30@gmail.com>
2024-10-19 18:39:33 +0100

    updated git action workflow DB migration command

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/config.js');
 
 const db = {};
 
