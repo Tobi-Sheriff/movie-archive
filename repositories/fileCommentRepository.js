2d4c5ae94bf4988e6e1d6a26ab88fa80d626a431
TobiSheriff <s.oluwatobiloba30@gmail.com>
2024-10-28 21:43:22 +0100

    Updated yml deploy file and removed try catch in repository and details. Also remove env checking in the seed generator

--- repositories/fileCommentRepository.js
+++ repositories/fileCommentRepository.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const { readJson } = require('../utils/fileUtils');
-const ExpressError = require('../utils/error');
 
 class FileCommentRepository {
   constructor(filePath) {
@@ -14,97 +13,71 @@ class FileCommentRepository {
   }
 
   async _fetch_comments() {
-    try {
-      return readJson(this.filePath);
-    } catch (err) {
-      throw new ExpressError(`Error checking if Comments exist: ${err.message}`, 404);
-    }
+    return readJson(this.filePath);
   }
 
   async countComments() {
-    try {
-      const comments = await this._fetch_comments();
-      return comments.length;
-    } catch (err) {
-      throw new ExpressError(`Error Counting Comments ${err.message}`);
-    }
+    const comments = await this._fetch_comments();
+    return comments.length;
   }
 
   async checkIfAnyCommentsExist() {
-    try {
-      return (await this.countComments()) > 0;
-    } catch (err) {
-      throw new ExpressError(`Error checking if Movies exist: ${err.message}`, 404);
-    }
+    return (await this.countComments()) > 0;
   }
 
   async getCommentsByMovieId(movieId, page, limit) {
-    try {
-      const comments = await this._fetch_comments();
-      const movieComments = comments.filter((comment) =>
-        comment.movie_id === parseInt(movieId)
-      );
+    const comments = await this._fetch_comments();
+    const movieComments = comments.filter((comment) =>
+      comment.movie_id === parseInt(movieId)
+    );
 
-      const totalItems = movieComments.length;
-      const totalPages = Math.ceil(totalItems / limit);
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
+    const totalCount = movieComments.length;
+    const totalPages = Math.ceil(totalCount / limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
-      const paginatedData = movieComments.slice(startIndex, endIndex);
-      return {
-        response: paginatedData,
-        pagination: {
-          totalItems,
-          totalPages,
-          pageSize: limit,
-          currentPage: page,
-        }
-      };
-    } catch (err) {
-      throw new ExpressError(`Error Getting Comments by movie ID: ${err.message}`, 404);
-    }
+    const paginatedData = movieComments.slice(startIndex, endIndex);
+    return {
+      response: paginatedData,
+      pagination: {
+        totalCount,
+        totalPages,
+        currentPage: page,
+        pageSize: limit,
+        hasNextPage: page < totalPages,
+        hasPreviousPage: page > 1,
+      }
+    };
   }
 
   async addAllComments(newComments) {
-    try {
-      const comments = await this._fetch_comments();
-      let commentMaxId = comments.length > 0 ? comments[comments.length - 1].id : 0;
+    const comments = await this._fetch_comments();
+    let commentMaxId = comments.length > 0 ? comments[comments.length - 1].id : 0;
 
-      newComments.forEach(comment => {
-        commentMaxId += 1;
-        comment.id = commentMaxId;
-      });
+    newComments.forEach(comment => {
+      commentMaxId += 1;
+      comment.id = commentMaxId;
+    });
 
-      comments.push(...newComments);
+    comments.push(...newComments);
 
-      await fs.promises.writeFile(this.filePath, JSON.stringify(comments));
-    } catch (err) {
-      throw new ExpressError(`Error Adding Multiple Comments: ${err.message}`, 404);
-    }
+    await fs.promises.writeFile(this.filePath, JSON.stringify(comments));
   }
 
   async addComment(newComment) {
-    try {
-      const comments = await this._fetch_comments();
+    const comments = await this._fetch_comments();
 
-      const maxId = comments.length > 0 ? comments[comments.length - 1].id : 0;
-      newComment.id = maxId + 1;
+    const maxId = comments.length > 0 ? comments[comments.length - 1].id : 0;
+    newComment.id = maxId + 1;
 
-      comments.push(newComment);
-      await fs.promises.writeFile(this.filePath, JSON.stringify(comments, null, 2));
+    comments.push(newComment);
+    await fs.promises.writeFile(this.filePath, JSON.stringify(comments, null, 2));
 
-      return newComment;
-    } catch (err) {
-      throw new ExpressError(`Error Adding a Comments: ${err.message}`, 404);
-    }
+    return newComment;
   }
 
   async deleteAllComments() {
-    try {
-      await fs.promises.writeFile(this.filePath, JSON.stringify([]))
-    } catch (err) {
-      throw new ExpressError(`Error Deleting Comments: ${err.message}`, 404);
-    }
+    await fs.promises.writeFile(this.filePath, JSON.stringify([]))
   }
 }
 
